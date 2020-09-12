deb9990642b5d44305f5e9dbcd5558eea547733f
Zihao Wang <zihao.wang1@ucdconnect.ie>
2020-09-12 21:12:25 +0800

    time efficiency improved

--- ACM/LC_134_Med.java
+++ ACM/LC_134_Med.java
@@ -4,8 +4,32 @@
 * @Website: www.wangzihao.org
 * @Date:    2020-09-11 21:10:12
 * @Last Modified by:   zihaowang
-* @Last Modified time: 2020-09-12 00:06:32
+* @Last Modified time: 2020-09-12 21:12:09
 */
+
+// Modified version, runtime beats 100%.
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sum;
+        for(int i = 0; i < gas.length; i++) {
+            gas[i] -= cost[i];
+        }
+        for(int i = 0; i < gas.length;i++) {
+            sum = 0;
+            for(int j = 0; j < gas.length; j++) {
+                sum += gas[(i + j) % gas.length];
+                if(sum < 0) {
+                    i += j;
+                    break;
+                }
+            }
+            if(sum >= 0) return i;
+        }
+        return -1;
+    }
+}
+
+// Original version, runtime 52ms.
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
 		int sum;
