b99b770c27265680266bedaca0a6f177c436b465
Zihao Wang <zihao.wang20@imperial.ac.uk>
2021-06-28 21:04:40 +0100

    LeetCode Daily Practice

--- ACM/LC_56_Med.java
+++ ACM/LC_56_Med.java
@@ -4,6 +4,29 @@
 * @Website: www.wangzihao.org
 * @Date:    2020-09-15 20:12:43
 * @Last Modified by:   zihaowang
-* @Last Modified time: 2020-09-15 20:58:44
+* @Last Modified time: 2021-06-28 21:04:22
 */
 
+class Solution {
+    	public int[][] merge(int[][] intervals) {
+        
+        List<int[]> results = new ArrayList<> ();
+        
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+       
+        
+        int[] currentInterval = intervals[0];
+        results.add(currentInterval);
+        
+        for(int i = 1; i < intervals.length; i++) {
+        	if(intervals[i][0] <= currentInterval[1]) 
+        		currentInterval[1] = Math.max(currentInterval[1], intervals[i][1]);
+        	else {
+        		currentInterval = intervals[i];
+        		results.add(currentInterval);
+            }
+        }
+        return results.toArray(new int[results.size()][]);
+    }
+	
+}
