7cac848edd04df3e74a530da8ee25a6c95b5e81d
Zihao Wang <zihao.wang20@imperial.ac.uk>
2021-08-10 17:53:49 +0100

    LeetCode Daily Exercise

--- ACM/LC_79_Med.java
+++ ACM/LC_79_Med.java
@@ -4,9 +4,53 @@
 * @Website: www.wangzihao.org
 * @Date:    2020-09-08 20:20:56
 * @Last Modified by:   zihaowang
-* @Last Modified time: 2020-09-08 20:21:02
+* @Last Modified time: 2021-08-10 17:53:33
 */
 
+// 2021 solution
+class Solution {
+    boolean[][] visited;
+    boolean result;
+    
+    public boolean exist(char[][] board, String word) {
+        visited = new boolean[board.length][board[0].length];
+        result = false;
+        
+        for(int y = 0; y < board.length; y++) {
+            for(int x = 0; x < board[0].length; x++) {
+                dfs(board, word, 0, y, x);
+                if(result) return true;
+            }
+        }
+        
+        return result;
+    }
+    
+    public void dfs(char[][] board, String word, int count, int y, int x) {
+        if(count == word.length()) {
+            result = true;
+            return;
+        }
+            
+        if(y >= board.length || y < 0 || x >= board[0].length || x < 0)
+            return;
+        if(visited[y][x])
+            return;
+        
+        if(board[y][x] == word.charAt(count)) {
+            visited[y][x] = true;
+            dfs(board, word, count + 1, y, x + 1);
+            dfs(board, word, count + 1, y, x - 1);
+            dfs(board, word, count + 1, y + 1, x);
+            dfs(board, word, count + 1, y - 1, x);
+            visited[y][x] = false;
+            
+            if(result) return;
+        }
+    }
+}
+
+// 2020 solution
 class Solution {
     boolean[][] access;
 	public boolean dfs(char[][] board, String word, int index, int row, int column) {
