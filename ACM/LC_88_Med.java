8f3bf11a01fc79be9da88bdb1b6cc3a86033fb2f
Zihao Wang <zihao.wang20@imperial.ac.uk>
2021-08-11 23:47:19 +0100

    LeetCode Daily Exercise

--- ACM/LC_88_Med.java
+++ ACM/LC_88_Med.java
@@ -1,3 +1,35 @@
+/*
+* @Author:  zihaowang
+* @Email:   zihao.wang1@ucdconnect.ie
+* @Website: www.wangzihao.org
+* @Date:    2021-08-11 23:28:33
+* @Last Modified by:   zihaowang
+* @Last Modified time: 2021-08-11 23:46:57
+*/
+
+// optimised solution
+class Solution {
+    public void merge(int[] A, int m, int[] B, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while(i >= 0 && j >= 0) {
+            if(A[i] > B[j])
+                A[k] = A[i--];
+            else 
+                A[k] = B[j--];
+            k--;
+        }
+        while(j >= 0) {
+            A[k] = B[j--];
+            k--;
+        }
+    }
+}
+
+
+// original solution
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int[] result = new int[m + n];
