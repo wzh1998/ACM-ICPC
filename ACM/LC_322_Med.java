0001cb7bef5bac558917b2e0b8bf1f74fca93f3f
Zihao Wang <zihao.wang20@imperial.ac.uk>
2021-08-23 16:43:45 +0100

    LeetCode Daily Exercise

--- ACM/LC_322_Med.java
+++ ACM/LC_322_Med.java
@@ -4,8 +4,37 @@
 * @Website: www.wangzihao.org
 * @Date:    2020-08-18 16:47:26
 * @Last Modified by:   zihaowang
-* @Last Modified time: 2020-08-18 17:08:22
+* @Last Modified time: 2021-08-23 16:43:26
 */
+
+// My 2021 solution
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(amount == 0) return 0;
+        int[] dp = new int[amount + 1];
+        
+        // Initialisation
+        for(int i = 0; i < dp.length; i++)
+            dp[i] = -1;
+        // Dynamic programming
+        for(int i = 0; i <= amount; i++) {
+            
+            for(int j = 0; j < coins.length; j++) {
+                int currentVal = coins[j];
+                
+                if(i < currentVal) continue;
+                
+                if(i == currentVal)
+                    dp[i] = 1;
+                else if(dp[i - currentVal] != -1)
+                    dp[i] = (dp[i] == -1) ? (dp[i - currentVal] + 1) : Math.min(dp[i], dp[i - currentVal] + 1);
+                
+            }
+        }
+        
+        return dp[amount];
+    }
+}
 
 class Solution {
     public int coinChange(int[] coins, int amount) {
