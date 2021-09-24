47455ff0e79c16c8624c0f2e575cd44368f9e739
Zihao Wang <zihao.wang20@imperial.ac.uk>
2021-09-25 00:51:49 +0100

    LeetCode Daily Exercise

--- ACM/LC_19_Med.java
+++ ACM/LC_19_Med.java
@@ -4,8 +4,32 @@
 * @Website: www.wangzihao.org
 * @Date:    2020-09-01 21:12:56
 * @Last Modified by:   zihaowang
-* @Last Modified time: 2020-09-01 21:28:31
+* @Last Modified time: 2021-09-25 00:51:33
 */
+// fast and slow pointer solution 2021
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow, fast;
+        ListNode virtualHead = new ListNode(0, head);
+        
+        fast = head;
+        
+        for(int i = 0; i < n; i++) 
+            fast = fast.next;
+        
+        slow = virtualHead;
+        while(fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        
+        return virtualHead.next;
+    }
+}
+
+
+// previous naive solution
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
 		List<ListNode> al = new ArrayList<> ();
