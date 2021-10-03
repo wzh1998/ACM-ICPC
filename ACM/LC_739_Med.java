4bf6c19c612f5c2fdb6777e7b2d647134e09006c
Zihao Wang <zihao.wang20@imperial.ac.uk>
2021-10-03 16:49:04 +0100

    LeetCode Daily Exercise

--- ACM/LC_739_Med.java
+++ ACM/LC_739_Med.java
@@ -1,5 +1,28 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<> ();
+        
+        for(int i = 0; i < temperatures.length; i++) {
+            if(stack.isEmpty() || temperatures[i] <= temperatures[stack.peek()]) {
+                stack.push(i);
+            }
+            else {
+                while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                    result[stack.peek()] = i - stack.peek();
+                    stack.pop();
+                }
+                stack.push(i);
+            }
+        }
+        
+        return result;
+    }
+}
+
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
         Stack<Integer> stack = new Stack<> ();
         int[] result = new int[temperatures.length];
         
